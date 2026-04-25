f98993bca34a793f52a2c4c006341a5eee2fffa8
willoughbytucker12 <elioperlman1012@gmail.com>
2026-04-25 07:01:42 +0000

    config 1: run 1+2

--- tabubu.cpp
+++ tabubu.cpp
@@ -6241,10 +6241,10 @@ int main(int argc, char* argv[]) {
              << ", iters_per_seg=" << CFG_MAX_ITER_PER_SEGMENT
              << ", no_improve=" << CFG_MAX_NO_IMPROVE << ")\n";
         if (n <= 20) {
-            CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 2);
+            CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 1);
             CFG_KNN_K = min(CFG_KNN_K, int(n));
         } else if (n <= 200) {
-            CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 2);
+            CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 1);
             CFG_KNN_K = min(CFG_KNN_K, int(n));
         } else {
             CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 1);
